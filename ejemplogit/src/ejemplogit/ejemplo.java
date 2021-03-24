c63c9c5e32a8ffb5d3a862fd4281ad52109b4f3c
Profe <moises.mascunan@juandegaray.es>
2021-03-24 11:32:03 +0100

    cambio2
    Signed-off-by: Profe <moises.mascunan@juandegaray.es>

--- ejemplogit/src/ejemplogit/ejemplo.java
+++ ejemplogit/src/ejemplogit/ejemplo.java
@@ -5,6 +5,8 @@ public class ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		System.out.println("Hola2");
+		System.out.println("Hola3");
 
 	}
 
